3a2e466c1cfaf74282b751cccf2ddf44af784b1c
Ashu <ashusinhacse@gmail.com>
2021-02-25 23:08:06 +0530

    Rest assured

---
+++ src/main/java/RestSample.java
@@ -0,0 +1,45 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+public class RestSample {
+
+	private String message;
+	
+	private String get;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getGet() {
+		return get;
+	}
+	public void setGet(String get) {
+		this.get = get;
+	}
+	
+
+
+
+
+@Test
+public void detail() {
+	RestSample rs = new RestSample();
+	rs.setMessage("Hi");
+	rs.setMessage("Ashu");
+	
+	
+	String res=given().body(rs).when().post("http://localhost:8080/RestSample").then().extract().response().asString();
+	System.out.println(res);
+	
+}
+
+
+
+
+
+
+}
